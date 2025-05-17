689a8e0a0df6169b7e331653004620434b7a5172
pf-rdeni <rdenirdeni@id.pepperl-fuchs.com>
2025-05-18 01:59:36 +0700

    Memperbarui tampilan halaman dashboard dengan mengganti kelas elemen kartu untuk konsistensi desain. Mengubah struktur kolom pada kalender untuk meningkatkan responsivitas dan pengalaman pengguna.

--- app/Views/backend/dashboard/index.php
+++ app/Views/backend/dashboard/index.php
@@ -156,9 +156,9 @@
                 </div>
                 <!-- /.row -->
                 <div class="row">
-                    <div class="col-md-3 col-sm-6 col-12">
+                    <div class="col-sm-12">
                         <!-- Calendar -->
-                        <div class="card bg-gradient-success">
+                        <div class="card bg-gradient-scundery">
                             <div class="card-header border-0">
 
                                 <h3 class="card-title">
@@ -167,22 +167,10 @@
                                 </h3>
                                 <!-- tools card -->
                                 <div class="card-tools">
-                                    <!-- button with a dropdown -->
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">
-                                            <i class="fas fa-bars"></i>
-                                        </button>
-                                        <div class="dropdown-menu" role="menu">
-                                            <a href="#" class="dropdown-item">Add new event</a>
-                                            <a href="#" class="dropdown-item">Clear events</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a href="#" class="dropdown-item">View calendar</a>
-                                        </div>
-                                    </div>
-                                    <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">
+                                    <button type="button" class="btn btn-scundery btn-sm" data-card-widget="collapse">
                                         <i class="fas fa-minus"></i>
                                     </button>
-                                    <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
+                                    <button type="button" class="btn btn-scundery btn-sm" data-card-widget="remove">
                                         <i class="fas fa-times"></i>
                                     </button>
                                 </div>
