7a83380d686d6f0a8fb298920ed6b032bdca162b
rdeni <rdeni@id.pepperl-fuchs.com>
2025-12-09 10:20:45 +0700

    feat: Update PDF paper size configuration in Rapor controller to custom folio size
    - Changed the paper size setting for PDF generation to a custom folio (F4) size, improving compatibility with specific printing requirements.

--- app/Controllers/Backend/Rapor.php
+++ app/Controllers/Backend/Rapor.php
@@ -914,7 +914,8 @@ class Rapor extends BaseController
             // Setup Dompdf dan render PDF
             $dompdf = $this->setupDompdfConfig();
             $dompdf->loadHtml($html);
-            $dompdf->setPaper('A4', 'portrait');
+            // set paper folio atau F4
+            $dompdf->setPaper([0, 0, 595.276, 935.433], 'portrait');
             $dompdf->render();
 
             // Output PDF
