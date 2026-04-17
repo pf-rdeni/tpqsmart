a0d39ba31d26db633e509a3911c8ef5f4abdd786
rdeni <rdeni@id.pepperl-fuchs.com>
2026-04-17 10:20:49 +0700

    feat: add custom data download view with configurable field mapping and filtering options

--- app/Views/backend/extra/showDownload.php
+++ app/Views/backend/extra/showDownload.php
@@ -15,26 +15,6 @@
 
 <?php $this->section('content'); ?>
 
-<!-- Content Header -->
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">
-                    <i class="fas fa-download text-warning mr-2"></i>
-                    Download Kustomisasi Data
-                </h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Home</a></li>
-                    <li class="breadcrumb-item">Extra</li>
-                    <li class="breadcrumb-item active">Download</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</div>
 
 <!-- Main Content -->
 <section class="content">
